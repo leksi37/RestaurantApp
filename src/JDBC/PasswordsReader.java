45e03e2786dd89d466416aa40b2d81a2c6b4f7fb
Aleksandra <aalexandrova11437@gmail.com>
2019-06-09 17:10:55 +0200

    prazno

--- src/JDBC/PasswordsReader.java
+++
@@ -1,9 +0,0 @@
-package JDBC;
-
-import basicClasses.Passwords;
-
-public interface PasswordsReader {
-    void add(Passwords p);
-    Passwords getPassword(String name);
-    void changePassword(Passwords password);
-}
